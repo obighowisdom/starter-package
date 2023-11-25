dc40ed4cd82fd03d57c88fe29da50cf70a5ab2d3
Aramenkhose Wisdom <aramenkhosewisdom@gmail.com>
2023-11-25 18:49:14 +0100

    few changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="card w-96 bg-base-100 shadow-xl image-full">
-        <figure>
-          <img
-            src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-
-            <button className="btn btn-primary">Buy Now</button>
-            
-          </div>
-        </div>
-      </div>
+   <h2>Welcome, Edit the pages and happy coding</h2>
     </main>
   );
 }
